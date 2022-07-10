38c6c79ddadf7a822e439635712cab85aca5d536
yeonsuBeak <soothbugger@gmail.com>
2022-07-10 14:25:52 +0900

    💄 Clean codes and style the component

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <input type="text" v-model="newTodoItem" />
-    <button @click="addTodo">plus</button>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" @keyup.enter="addTodo" />
+    <span class="addContainer" @click="addTodo">
+      <i class="fa fa-plus addBtn"></i>
+    </span>
   </div>
 </template>
 
@@ -23,3 +25,41 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+
+.inputBox {
+  position: relative;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+  background: #fff;
+}
+
+.inputBox input {
+  margin-left: 1.5rem;
+  margin-right: 4.5rem;
+  width: calc(100% - 6rem);
+  font-size: 0.9rem;
+  border-style: none;
+}
+
+.addContainer {
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  cursor: pointer;
+}
+
+.addBtn {
+  color: #fff;
+  vertical-align: middle;
+}
+</style>
