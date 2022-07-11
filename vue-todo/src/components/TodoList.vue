1a9ad493560cf0312813633922e9b506bb5e78c0
yeonsuBeak <soothbugger@gmail.com>
2022-07-11 12:47:56 +0900

    🚧 Delete todoItem when clicking removeBtn

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <ul>
-      <li v-for="todoItem in todoItems" :key="todoItem">{{ todoItem }}</li>
+      <li v-for="(todoItem, index) in todoItems" :key="todoItem" class="shadow">
+        {{ todoItem }}
+
+        <span class="removeBtn" @click="removeTodo(todoItem, index)">
+          <i class="fa fa-trash"></i>
+        </span>
+      </li>
     </ul>
   </div>
 </template>
@@ -13,15 +19,59 @@ export default {
       todoItems: [],
     };
   },
+  methods: {
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    },
+  },
   created() {
     if (localStorage.length > 0) {
       for (let i = 0; i < localStorage.length; i++) {
         this.todoItems.push(localStorage.key(i));
-        // console.log(localStorage.key(i));
       }
     }
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0;
+  margin-top: 0;
+  text-align: left;
+}
+
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: #fff;
+  border-radius: 5px;
+}
+
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+  cursor: pointer;
+}
+
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+
+.checkBtnCompleted {
+  color: #b3adad;
+}
+
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+</style>
